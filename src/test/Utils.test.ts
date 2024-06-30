b97e5ec84d20bd27a5cd8df08c453ee16ed2bb67
Jacob David Diehl <jakediehl17@gmail.com>
2024-06-30 12:10:03 -0600

    configured jest and typescript. Wrote first test for a function

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,20 @@
+import { toUpperCase } from "../app/Utils"
+
+
+
+
+
+describe('Utils test suite',()=>{
+
+    test('should return uppercase',()=>{
+        const result = toUpperCase('abc');
+        expect(result).toBe('ABC');
+    })
+
+
+
+})
+
+
+
+
